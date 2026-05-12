9ff2f1f43aa184fce5f3fb808d73b887d7a44f95
bsmitt <bsmith27@medford.k12.ma.us>
2026-05-12 14:17:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -42,27 +42,37 @@ class encoderValue:
             return raw
         return None
 
-i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
-print(i2c.scan())
+mcpI2C = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
+mcp = mcp23017.MCP23017(mcpI2C, 0x20)
 
 
-#mcpI2C = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)
-#print(mcpI2C.scan())
 
-#mcp = mcp23017.MCP23017(mcpI2C, 0x20)
+baseDir = 0
+shoulderDir = 1
+elbowDir = 2
+wrist_rightDir = 3
+wrist_leftDir = 4
+gripperDir = 5
+
+mcp.pin(baseDir, mode=0)
+mcp.pin(shoulderDir, mode=0)
+mcp.pin(elbowDir, mode=0)
+mcp.pin(wrist_rightDir, mode=0)
+mcp.pin(wrist_leftDir, mode=0)
+mcp.pin(gripperDir, mode=0)
 
 baseEncoder=encoderValue(16)
 shoulderEncoder=encoderValue(18)
 elbowEncoder=encoderValue(20)
-wristrightEncoder=encoderValue(14)
-wristleftEncoder=encoderValue(12)
+wrist_rightEncoder=encoderValue(14)
+wrist_leftEncoder=encoderValue(12)
 gripperEncoder=encoderValue(10)
 
 basePWM = PWM(Pin(9), freq = 1000, duty_u16=65535)
 shoulderPWM = PWM(Pin(8), freq = 1000, duty_u16=65535)
 elbowPWM = PWM(Pin(7), freq = 1000, duty_u16=65535)
-wristrightPWM = PWM(Pin(6), freq = 1000, duty_u16=65535)
-wristleftPWM = PWM(Pin(5), freq = 1000, duty_u16=65535)
+wrist_rightPWM = PWM(Pin(6), freq = 1000, duty_u16=65535)
+wrist_leftPWM = PWM(Pin(5), freq = 1000, duty_u16=65535)
 gripperPWM = PWM(Pin(4), freq = 1000, duty_u16=65535)
 
 
@@ -70,15 +80,16 @@ while True:
     baseCount = baseEncoder.getCount()
     shoulderCount = shoulderEncoder.getCount()
     elbowCount = elbowEncoder.getCount()
-    wristrightCount = wristrightEncoder.getCount()
-    wristleftCount = wristleftEncoder.getCount()
+    wrist_rightCount = wrist_rightEncoder.getCount()
+    wrist_leftCount = wrist_leftEncoder.getCount()
     gripperCount = gripperEncoder.getCount()
     
-    basePWM.duty_u16(65535)
+    basePWM.duty_u16(0)
     shoulderPWM.duty_u16(0)
     elbowPWM.duty_u16(0)
-    wristrightPWM.duty_u16(0)
-    wristleftPWM.duty_u16(0)
+    wrist_rightPWM.duty_u16(65535)
+    wrist_leftPWM.duty_u16(65535)
     gripperPWM.duty_u16(0)
     
-    
+    mcp.pin(wrist_leftDir, value=1)
+    mcp.pin(wrist_rightDir, value=0)
